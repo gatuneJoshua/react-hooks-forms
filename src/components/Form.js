41843807d5e19ca688675f4fb852d32bc6737566
Joshua <joshua.gatune@student.moringaschool.com>
2023-07-20 12:22:42 +0300

    initial commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,6 +4,13 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+   function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
   return (
     <form>
       <input type="text" value={firstName} />
